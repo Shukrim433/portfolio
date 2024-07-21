75f087ad3b4db5756f14c98081f742a7a634c708
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-07-21 16:58:36 +0100

    set up react router with page components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// import all pages
+import App from "./App";
+import ErrorPage from "./pages/ErrorPage";
+import AboutMePage from "./pages/AboutMePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ContactMePage from "./pages/ContactMePage";
+import LandingPage from "./pages/LandingPage";
+
+const router = createBrowserRouter([
+  // define all the accessible routes, and which component(page) responds to which URL
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "projects",
+        element: <ProjectsPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactMePage />,
+      },
+      {
+        path: "about",
+        element: <AboutMePage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
