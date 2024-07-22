60a95a0a27121ea06fc0478b0fc334ac45adeb49
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-07-22 22:45:45 +0100

    dont include header and footer components for landing page ie "/" url

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
+  const location = useLocation()
+  const isLandingPage = location.pathname === '/'
   return (
     <>
-      <Header />
+      {!isLandingPage && <Header />}
       {/* Outlet component will conditionally show the appropriate page component based on the URL */}
       <div className="body">
         <Outlet />
       </div>
-      <Footer />
+      {!isLandingPage && <Footer />}
     </>
   );
 }
