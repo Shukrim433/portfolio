9cd524d9f5ab65ebf40a012ee1077fb59f07ea5f
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-07-22 22:44:19 +0100

    landing page functionality done

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Typewriter } from "react-simple-typewriter";
+import "../styles/LandingPage.css";
+
+export default function LandingPage() {
+  const [isTypingComplete, setIsTypingComplete] = useState(false);
+  const navigate = useNavigate();
+
+  // after 7.5 seconds show the btn
+  setTimeout(() => {
+    setIsTypingComplete(true);
+  }, 7500);
+
+  const handleClick = () => {
+    navigate("/projects");
+  }
+  document.body.style = "background: #fffcf2;"; // beige
+
+  return (
+    <div className="landing-page">
+      <div className="type-writer">
+        <h1 style={{ color: "#283618" }}>
+          <Typewriter
+            words={[
+              "Hi, my name is Shukri Mohamed. I'm a full stack web developer and welcome to my portfolio!",
+            ]}
+            loop={1}
+            typeSpeed={70}
+            delaySpeed={1000}
+          />
+        </h1>
+      </div>
+      {isTypingComplete && (
+        <div className="lets-go">
+          <button onClick={handleClick}>Let's go!</button>
+        </div>
+      )}
+    </div>
+  );
+}
