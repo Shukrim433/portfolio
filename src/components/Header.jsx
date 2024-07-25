044ee9365f2490f49417c9499d15f58064bc5ae0
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-07-25 14:37:47 +0100

    header, navbar and hamburger done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,24 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./UI/NavBar";
-import "../styles/Header.css";
+/* import "../styles/Header.css"; */
 
 export default function Header() {
-
   return (
-    <header className="header">
-      <div>
-        <h1>Shukri Mohamed</h1>
-      </div>
-      <Navbar
-        links={[
-          <Link key={1} className="nav-link" to="/projects">
-            Projects
-          </Link>,
-          <Link key={2} className="nav-link" to="/contact">
-            Contact Me
-          </Link>,
-          <Link key={3} className="nav-link" to="/about">
-            About Me
-          </Link>,
-        ]}
-      />
-    </header>
+    <React.Fragment>
+        <Navbar
+          links={[
+            <Link key={1} className="nav-link" to="/projects">
+              Projects
+            </Link>,
+            <Link key={2} className="nav-link" to="/contact">
+              Contact Me
+            </Link>,
+            <Link key={3} className="nav-link" to="/about">
+              About Me
+            </Link>,
+          ]}
+        />
+    </React.Fragment>
   );
 }
--- src/components/UI/NavBar.jsx
+++ src/components/UI/NavBar.jsx
@@ -1,13 +1,45 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "../../styles/NavBar.css";
 
 export default function Navbar({ links }) {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
   return (
-    <nav className="navbar">
-      <ul>
-        {links.map((link, index) => (
-          <li key={index}>{link}</li>
-        ))}
-      </ul>
-    </nav>
+    <header>
+      <h1>Shukri Mohamed</h1>
+
+      <nav ref={navRef} className="navbar">
+        <ul>
+          {links.map((link, index) => (
+            <a key={index}>{link}</a>
+          ))}
+        </ul>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn " onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
   );
 }
+
+/* <FaTimes />  button to close nav bar on small screens */
+/* <FaBars /> {/* button to open nav bar on small screens */
+/* useRef hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated.
+ lets you reference a value that's not needed for rendering. */
+
+/* showNavbar - each time this function is called, we will add or remove (toggle) the  
+ the "responsive_nav" classname from the classList of the <nav> element
+ */
+// essentially a diff way of doing the use state showNave=false + toggle nav function
+
+/* when your on a small screen you will only see the FaBars icon, when you click it
+it adds the  "responsive_nav" className to the <nav> element, and the <nav> will take up the whole screen
+and now the FaTimes icon is the only icon on the screen and when you click that it will remove the "responsive_nav"
+from the <nav> element, returning back to normal screen*/
