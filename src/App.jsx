c25000dca25e1ab0baf6d016c7760d92bc900954
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-07-25 14:36:55 +0100

    added vackground color to App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import './styles/App.css'
 
 function App() {
   const location = useLocation()
   const isLandingPage = location.pathname === '/'
+  document.body.style.background = '#f8f4e7'; // beige
   return (
-    <>
+    <div className="App">
       {!isLandingPage && <Header />}
       {/* Outlet component will conditionally show the appropriate page component based on the URL */}
       <div className="body">
         <Outlet />
       </div>
       {!isLandingPage && <Footer />}
-    </>
+    </div>
   );
 }
 
