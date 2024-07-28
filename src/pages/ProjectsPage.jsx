3ad4fd467ab574fc143de47bb891fdaaf6081128
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-07-28 16:04:00 +0100

    added typewriter and hero div

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,5 +1,7 @@
 import ProjectList from "../components/UI/ProjectList";
 import ImageSlider from "../components/ImageSlider";
+import { Typewriter } from "react-simple-typewriter";
+import hero1 from "../imgs/hero1.png"
 import { FaReact, FaCss3Alt, FaNodeJs, FaBootstrap } from "react-icons/fa";
 import { TbBrandJavascript, TbSql } from "react-icons/tb";
 import { IoLogoHtml5 } from "react-icons/io";
@@ -20,79 +22,79 @@ export default function ProjectsPage() {
   console.log(burgerClicked, "ProjectsPage comp");
   const slides = [
     {
-      url: "http://localhost:3000/image-1.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "beach",
       skill: "React",
       icon: <FaReact />,
     },
     {
-      url: "http://localhost:3000/image-2.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "boat",
       skill: "JavaScript",
       icon: <TbBrandJavascript />,
     },
     {
-      url: "http://localhost:3000/image-3.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "forest",
       skill: "CSS",
       icon: <FaCss3Alt />,
     },
     {
-      url: "http://localhost:3000/image-4.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "city",
       skill: "HTML",
       icon: <IoLogoHtml5 />,
     },
     {
-      url: "http://localhost:3000/image-5.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "italy",
       skill: "SQL",
       icon: <TbSql />,
     },
     {
-      url: "http://localhost:3000/image-1.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "beach",
       skill: "PostgreSQL",
       icon: <BiLogoPostgresql />,
     },
     {
-      url: "http://localhost:3000/image-2.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "boat",
       skill: "MongoDB",
       icon: <SiMongodb />,
     },
     {
-      url: "http://localhost:3000/image-3.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "forest",
       skill: "Mongoose",
       icon: <SiMongodb />,
     },
     {
-      url: "http://localhost:3000/image-4.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "city",
       skill: "Express JS",
       icon: <SiExpress />,
     },
     {
-      url: "http://localhost:3000/image-5.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "italy",
       skill: "Node JS",
       icon: <FaNodeJs />,
     },
     {
-      url: "http://localhost:3000/image-5.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "italy",
       skill: "Handlebars",
       icon: <SiHandlebarsdotjs />,
     },
     {
-      url: "http://localhost:3000/image-5.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "italy",
       skill: "Jquery",
       icon: <SiJquery />,
     },
     {
-      url: "http://localhost:3000/image-5.jpg",
+      url: "http://localhost:3000/image-1.jpeg",
       title: "italy",
       skill: "Bootstrap",
       icon: <FaBootstrap />,
@@ -101,8 +103,20 @@ export default function ProjectsPage() {
 
   return (
     <>
-      <div className="img-slider-container">
-        {burgerClicked === false && <ImageSlider slides={slides} />}
+      <div className="hero" style={{ backgroundImage: `url(${hero1})` }}>
+        <div className="img-slider-container">
+          {burgerClicked === false && <ImageSlider slides={slides} />}
+        </div>
+        <h1 className="hero-text">
+          <Typewriter
+            words={["These are my technical skills and projects"]}
+            loop={1}
+            typeSpeed={70}
+            delaySpeed={1000}
+            cursor
+          />
+        </h1>
+        
       </div>
 
       <div className="projects-container">
