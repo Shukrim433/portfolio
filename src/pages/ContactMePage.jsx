70499ab9c1a89c2702795290dc48b61931cd9af8
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-07-28 16:01:55 +0100

    contact me page and cv pdf

--- src/pages/ContactMePage.jsx
+++ src/pages/ContactMePage.jsx
@@ -1,5 +1,24 @@
+import CV from "../assets/CV.pdf";
+import PDF from "../imgs/PDF.png";
+import "../styles/ContactMe.css";
+
 export default function ContactMePage() {
-    return (
-        <div>ContactMePage</div>
-    )
+  return (
+    <div className="contact-me">
+      <div className="cv-card">
+        <img src={PDF} className="pdf-img"></img>
+        <div className="cv-text">
+          <button>
+            <a href={CV} download="CV">
+              Download my CV!
+            </a>
+          </button>
+        </div>
+      </div>
+      <div className="contact-me-text">
+      <p>Email: Shukrim433@gmail.com</p>
+      <p> Github: github.com/Shukrim433/</p>
+      </div>
+    </div>
+  );
 }
