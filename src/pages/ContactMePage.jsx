11f3c9297e0a46decc29ceb8c9400fce6de37f55
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-07-31 12:09:54 +0100

    contact me page done

--- src/pages/ContactMePage.jsx
+++ src/pages/ContactMePage.jsx
@@ -1,24 +1,50 @@
 import CV from "../assets/CV.pdf";
 import PDF from "../imgs/PDF.png";
+import { CiLinkedin } from "react-icons/ci";
+import { MdOutlineEmail } from "react-icons/md";
 import "../styles/ContactMe.css";
 
 export default function ContactMePage() {
   return (
     <div className="contact-me">
+      <div className="contact-header">
+        <h2>
+          {" "}
+          <b>
+            {" "}
+            <i>Get in touch with me: </i>
+          </b>
+        </h2>
+      </div>
+
       <div className="cv-card">
-        <img src={PDF} className="pdf-img"></img>
+        
+
         <div className="cv-text">
           <button>
             <a href={CV} download="CV">
-              Download my CV!
+              Download my CV
             </a>
           </button>
         </div>
+
+        <div className="contact-me-text">
+        <p>
+          <b>
+            <MdOutlineEmail /> Shukrim433@gmail.com{" "}
+          </b>
+        </p>
+        <p>
+          <b>
+            <a href="www.linkedin.com/in/shukri-mohamed-5bb482283">
+              {" "}
+              <CiLinkedin /> LinkedIn
+            </a>{" "}
+          </b>
+        </p>
       </div>
-      <div className="contact-me-text">
-      <p>Email: Shukrim433@gmail.com</p>
-      <p> Github: github.com/Shukrim433/</p>
       </div>
+      
     </div>
   );
 }
