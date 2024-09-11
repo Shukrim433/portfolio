33a7a037fa4a7c3feb69fa14064dc3bdb653c556
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-09-11 15:14:16 +0100

    updated menu icon and x icon

--- src/components/UI/NavBar.jsx
+++ src/components/UI/NavBar.jsx
@@ -34,18 +34,43 @@ export default function Navbar({ links }) {
       <nav ref={navRef} className="navbar">
         <ul>
           {links.map((link, index) => (
-            <li key={index}>
-              {" "}
-              {link}
-            </li>
+            <li key={index}> {link}</li>
           ))}
         </ul>
         <button className="nav-btn nav-close-btn" onClick={handleCloseClick}>
-          <FaTimes />
+          {/* "X" ICON */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="x-icon size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
         </button>
       </nav>
       <button className="nav-btn " onClick={handleBurgerClick}>
-        <FaBars />
+        {/* MENU BARS ICON */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="menu-icon size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 9h16.5m-16.5 6.75h16.5"
+          />
+        </svg>
       </button>
     </header>
   );
